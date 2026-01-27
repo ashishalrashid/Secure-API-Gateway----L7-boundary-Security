12f4ae89300d237c1e786a2e3152911367054a55
Rashid <ashishalrashid@gmail.com>
2026-01-27 17:00:38 +0530

    Api guards implemented

---
+++ secure-api-gateway/src/common/guards/api-key/api-key.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import type { Request } from "express";
+
+
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate{
+  canActivate(context: ExecutionContext): boolean {
+    const req =context.switchToHttp().getRequest<Request>();
+
+    const apiKey =req.headers['x-api-key'];
+
+    if (!apiKey){
+      throw new UnauthorizedException('Missing API key')
+    }
+
+    if (apiKey!=="dummy_key"){
+      throw new UnauthorizedException("Invalid API key")
+    }
+
+    return true;
+  } 
+      
+}
--- secure-api-gateway/src/data-plane/gateway/gateway.controller.ts
+++ secure-api-gateway/src/data-plane/gateway/gateway.controller.ts
@@ -3,13 +3,17 @@ import {
   All,
   Req,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import type { Request, Response } from 'express';
 import * as httpProxy from 'http-proxy';
+import { userInfo } from 'node:os';
+import { ApiKeyGuard } from 'src/common/guards/api-key/api-key.guard';
 
 const proxy = httpProxy.createProxyServer({});
 
 @Controller('api')
+@UseGuards(ApiKeyGuard)
 export class GatewayController {
   private readonly upstream = 'http://localhost:4000';
 
