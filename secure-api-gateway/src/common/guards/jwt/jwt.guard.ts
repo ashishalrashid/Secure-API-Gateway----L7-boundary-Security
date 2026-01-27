9b709309ad833a2870254e7b95d14991be95264c
Rashid <ashishalrashid@gmail.com>
2026-01-28 01:56:32 +0530

    JWKS support jwt guards

--- secure-api-gateway/src/common/guards/jwt/jwt.guard.ts
+++ secure-api-gateway/src/common/guards/jwt/jwt.guard.ts
@@ -1,29 +1,39 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
-import { Observable } from "rxjs";
+import type { Request } from "express";
+import { createRemoteJWKSet,jwtVerify } from 'jose';
+import { Subject } from "rxjs";
 
+const jwks= createRemoteJWKSet(
+  new URL(process.env.IDP_JWKS_URI!)
+);
 
 @Injectable()
 export class Jwtguard implements CanActivate{
-  canActivate(context: ExecutionContext): boolean {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
       const req =context.switchToHttp().getRequest<Request>();
 
-      const authHeader =req.headers['authorization'];
+      const auth =req.headers['authorization'];
 
-      if (!authHeader){
-        throw new UnauthorizedException('Missing auth header')
+      if (!auth|| auth.startsWith('Bearer ')){
+        throw new UnauthorizedException('Missing authorization')
       }
 
-      if (!authHeader.startsWith('Bearer ')){
-        throw new UnauthorizedException('Invalid auth')
+      const token =auth.slice(7); 
+      try{
+        const {payload}= await jwtVerify(token,jwks,{
+          issuer:process.env.IDP_ISSUER,
+          audience:process.env.IDP_AUDIENCE,
+        });
+        (req as any).identity ={
+          subject:payload.sub,
+          tenantId:payload.tenant_id,
+          claims: payload,
+        };
+        return true;
+
+      }catch(err){
+        throw new UnauthorizedException('Invalid JWT');
       }
 
-      const token =authHeader.slice(7);
-
-      if (!token){
-        throw new UnauthorizedException('Missing Jwt')
-      }
-
-      //add JWT validations
-      return true;
   }
 }
